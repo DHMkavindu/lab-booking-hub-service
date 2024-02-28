12a93f150761b6c0397f96ce2552ae48c2591142
Kavindu Dilshan <98866207+dhananjaya20@users.noreply.github.com>
2024-02-28 11:34:43 +0530

    Add Admin Dto

---
+++ src/main/java/lk/kavi/labbookinghub/dto/AdminDTO.java
@@ -0,0 +1,15 @@
+package lk.kavi.labbookinghub.dto;
+
+import lombok.Data;
+
+@Data
+public class AdminDTO {
+
+    private Integer id;
+
+    private String name;
+
+    private String password;
+
+    private String NIC;
+}
